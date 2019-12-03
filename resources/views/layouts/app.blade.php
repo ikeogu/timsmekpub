a7b49de35caeb5356cb7064371e74c68d09372fc
Ikeogu <ikeogu322@gmail.com>
2019-12-04 00:30:00 +0100

    adding to herku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,14 @@
         
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- <link rel="manifest" href="{{asset('manifest.json')}}"> --}}
+    {{-- <link rel="manifest" href="{{secure_asset('manifest.json')}}"> --}}
     <link rel="apple-touch-icon" href="icon.png">
-    <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
-    <link rel="stylesheet"  href="{{asset('css/slick.css')}}" />
-    <link href="{{asset('css/ionicons.min.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/normalize.css')}}">
+    <link rel="stylesheet"  href="{{secure_asset('css/slick.css')}}" />
+    <link href="{{secure_asset('css/ionicons.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{secure_asset('css/bootstrap.min.css')}}">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/main.css')}}">
     <meta name="theme-color" content="#fafafa">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -49,7 +49,7 @@
 		<header>
 			<div id="top-header">
 				<div class="branding">
-					<img src="{{asset('img/logo.png')}}" alt="logo" class="img-fluid" width="400">
+					<img src="{{secure_asset('img/logo.png')}}" alt="logo" class="img-fluid" width="400">
 				</div>
 				<div id="info">
 					<p><i class="icon ion-logo-whatsapp"></i> <i class="icon ion-md-call"></i></i> <span
@@ -119,7 +119,7 @@
 					<div class="row ">
 						<div class="col-md-4 m-0">
 							<div class="time-logo">
-								<img src="{{asset('img/logo.png')}}" alt="" class="img-fluid" width="300">
+								<img src="{{secure_asset('img/logo.png')}}" alt="" class="img-fluid" width="300">
 							</div>
 							<p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto id magnam fugit officia. Nam qui odit vero
 								quia pariatur enim dolore voluptatem quibusdam ducimus dignissimos ex ea, eveniet consectetur repellendus.
@@ -168,19 +168,19 @@
 		
 	
 	
-		<script src="{{asset('js/vendor/modernizr-3.7.1.min.js')}}"></script>
-		<script src="{{asset('/js/jquery-3.4.1.min.js')}}"
+		<script src="{{secure_asset('js/vendor/modernizr-3.7.1.min.js')}}"></script>
+		<script src="{{secure_asset('/js/jquery-3.4.1.min.js')}}"
 			 crossorigin="anonymous"></script>
 		<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
-		<script type="text/javascript" src="{{asset('js/slick.min.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/slick.min.js')}}"></script>
 		<script src="js/plugins.js"></script>
-		<script src="{{asset('js/jquery-3.3.1.slim.min.js')}}"
+		<script src="{{secure_asset('js/jquery-3.3.1.slim.min.js')}}"
 			></script>
-		<script src="{{asset('js/popper.min.js')}}"
+		<script src="{{secure_asset('js/popper.min.js')}}"
 			></script>
-		<script src="{{asset('js/bootstrap.min.js')}}"
+		<script src="{{secure_asset('js/bootstrap.min.js')}}"
 			></script>
-		<script src="{{asset('js/main.js')}}"></script>
+		<script src="{{secure_asset('js/main.js')}}"></script>
 	
 		<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 		<script>
