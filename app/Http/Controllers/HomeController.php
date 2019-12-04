80f94188d8efb2238f0fc06e8b9ef37531669ab6
Emmanuel chidera <ikeogu322@gmail.com>
2019-12-04 09:47:46 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,14 +32,14 @@ class HomeController extends Controller
         if(Auth::user()->isAdmin== 1||Auth::user()->isAdmin== 2){
             return redirect(route('admindashboard'));
         }   
-        $blog = Blog::all()->take(3)->get();
-        $book = Publish::all()->take(10)->get();
+        $blog = Blog::all();
+        $book = Publish::all();
         $recent = Publish::latest()->take(8)->get();
         return view('pages/index',['recent'=>$recent,'blog'=>$blog, 'book'=>$book,]); 
     }
     public function first(){
-        $blog = Blog::all()->take(3)->get();
-        $book = Publish::all()->take(10)->get();
+        $blog = Blog::all();
+        $book = Publish::all();
         $recent = Publish::latest()->take(8)->get();
         // dd($recent);
         return view('pages/index',['blog'=>$blog, 'book'=>$book,'recent'=>$recent]);
